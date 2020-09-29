d653fe6cac30151fad3cca4d7c88fee806edbfab
ORolandoS <otaku_soft@yahoo.es>
2020-09-29 00:18:03 -0400

    Adicionar la nueva plantilla

---
+++ application/views/plantilla/cabecerahtml.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap Admin Theme v3</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- styles -->
+    <link href="css/styles.css" rel="stylesheet">
---
+++ application/views/plantilla/piehtml.php
@@ -0,0 +1,21 @@
+</div><!--Fin de Contenido-->
+		  		</div>
+
+		  	</div>
+		  </div>
+		</div>
+    </div>
+
+    <footer>
+         <div class="container">
+            <div class="copy text-center">
+               Copyright 2014 <a href="#">Website</a>
+            </div>
+         </div>
+      </footer>
+
+    <script src="js/jquery.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/custom.js"></script>
+  </body>
+</html>
