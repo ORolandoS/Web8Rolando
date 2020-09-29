71b5d24e5e23158c5042cce146cda556a145e324
ORolandoS <otaku_soft@yahoo.es>
2020-09-29 00:32:58 -0400

    Modificaciones de la plantilla y adicion de .htaccess

--- application/views/plantilla/cabecerahtml.php
+++ application/views/plantilla/cabecerahtml.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Bootstrap Admin Theme v3</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <!-- styles -->
-    <link href="css/styles.css" rel="stylesheet">
+
+<head>
+  <title>Bootstrap Admin Theme v3</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Bootstrap -->
+  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- styles -->
+  <link href="css/styles.css" rel="stylesheet">
--- application/views/plantilla/piehtml.php
+++ application/views/plantilla/piehtml.php
@@ -1,21 +1,23 @@
-</div><!--Fin de Contenido-->
-		  		</div>
+</div>
+<!--Fin de Contenido-->
+</div>
 
-		  	</div>
-		  </div>
-		</div>
+</div>
+</div>
+</div>
+</div>
+
+<footer>
+  <div class="container">
+    <div class="copy text-center">
+      Derechos de Autor <a href="#"> - Sistema creado por Rolando Aruquipa</a>
     </div>
+  </div>
+</footer>
 
-    <footer>
-         <div class="container">
-            <div class="copy text-center">
-               Copyright 2014 <a href="#">Website</a>
-            </div>
-         </div>
-      </footer>
+<script src="js/jquery.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="js/custom.js"></script>
+</body>
 
-    <script src="js/jquery.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-    <script src="js/custom.js"></script>
-  </body>
 </html>
