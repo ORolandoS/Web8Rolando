c50378405075648f4269506386edabbb8943c6d6
ORolandoS <otaku_soft@yahoo.es>
2020-09-30 01:17:52 -0400

    Venta Producto

--- application/views/plantilla/cabecera.php
+++ application/views/plantilla/cabecera.php
@@ -71,6 +71,29 @@
               </li>
               <li class="submenu">
                 <a href="#">
+                  <i class="glyphicon glyphicon-list"></i> Compras
+                  <span class="caret pull-right"></span>
+                </a>
+                <!-- Sub menu -->
+                <ul>
+                  <li><a href="<?= base_url(); ?>compra/nuevo">Nuevo Compra</a></li>
+                  <li><a href="<?= base_url(); ?>compra/listar">Listar Compras</a></li>
+                </ul>
+              </li>
+
+              <li class="submenu">
+                <a href="#">
+                  <i class="glyphicon glyphicon-list"></i> Venta
+                  <span class="caret pull-right"></span>
+                </a>
+                <!-- Sub menu -->
+                <ul>
+                  <li><a href="<?= base_url(); ?>venta/nuevo">Nueva Venta</a></li>
+                  <li><a href="<?= base_url(); ?>venta/listar">Listar Ventas</a></li>
+                </ul>
+              </li>
+              <li class="submenu">
+                <a href="#">
                   <i class="glyphicon glyphicon-user"></i> Usuario
                   <span class="caret pull-right"></span>
                 </a>
@@ -80,6 +103,7 @@
                   <li><a href="<?=base_url();?>usuario/listar">Listar usuario</a></li>
                 </ul>
               </li>
+
             </ul>
           </div>
         </div>
--- application/views/plantilla/piehtml.php
+++ application/views/plantilla/piehtml.php
@@ -14,10 +14,23 @@
     </div>
   </div>
 </footer>
-
+<script>
+    const base_url = "<?= base_url(); ?>";
+</script>
 <script src="<?=base_url();?>js/jquery.js"></script>
 <script src="<?=base_url();?>bootstrap/js/bootstrap.min.js"></script>
 <script src="<?=base_url();?>js/custom.js"></script>
+<?php
+  if (isset($archivoJSCargar))
+  {
+      foreach ($archivoJSCargar as $js)
+      {
+?>
+<script src="<?= base_url(); ?><?= $js; ?>"></script>
+<?php
+      }
+  }
+?>
 </body>
 
 </html>
